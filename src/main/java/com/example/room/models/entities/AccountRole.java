fe9bac4b21201fd29055799194e77aba2028a6b5
Sonpn <akau911x@gmail.com>
2021-06-09 17:14:34 +0700

    first commit

---
+++ src/main/java/com/example/room/models/entities/AccountRole.java
@@ -0,0 +1,42 @@
+package com.example.room.models.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "account_role")
+public class AccountRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_id")
+    private Long roleId;
+
+    @Column(name = "account_id")
+    private Long accountId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+}
